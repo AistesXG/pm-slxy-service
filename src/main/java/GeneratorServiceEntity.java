f6a7f88c75f3e3b40180f1bc1843bbe8d9e18c92
lenovo <1003581879@qq.com>
2018-02-27 10:46:22 +0800

    Project framework

---
+++ src/main/java/GeneratorServiceEntity.java
@@ -0,0 +1,70 @@
+
+
+/**
+ * @author furg@senthink.com
+ * @date 2018/1/23
+ */
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.junit.Test;
+
+public class GeneratorServiceEntity {
+    @Test
+    public void generateCode() {
+        String packageName = "com.pm.slxy";
+        boolean serviceNameStartWithI = false;//user -> UserService, 设置成true: user -> IUserService
+        generateByTables(serviceNameStartWithI, packageName, "tb_campus");
+    }
+
+    private void generateByTables(boolean serviceNameStartWithI, String packageName, String... tableNames) {
+
+        GlobalConfig config = new GlobalConfig();
+        String dbUrl = "jdbc:mysql://localhost:3306/pm";
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL)
+                .setUrl(dbUrl)
+                .setUsername("root")
+                .setPassword("123456")
+                .setDriverName("com.mysql.jdbc.Driver");
+
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig
+                .setCapitalMode(true)
+                .setEntityLombokModel(false)
+                .setDbColumnUnderline(true)
+                .setNaming(NamingStrategy.underline_to_camel)
+                .setInclude(tableNames);//修改替换成你需要的表名，多个表名传数组
+
+        config.setActiveRecord(false)
+                .setAuthor("付荣刚")
+                .setOutputDir("D:\\code")
+                .setFileOverride(true);
+
+        if (!serviceNameStartWithI) {
+            config.setServiceName("%sService");
+        }
+
+        new AutoGenerator()
+                .setGlobalConfig(config)
+                .setDataSource(dataSourceConfig)
+                .setStrategy(strategyConfig)
+                .setPackageInfo(
+                        new PackageConfig()
+                                .setParent(packageName)
+                                .setController("controller")
+                                .setEntity("entity")
+                                .setService("service")
+                                .setServiceImpl("serviceImpl")
+                ).execute();
+    }
+
+    private void generateByTables(String packageName, String... tableNames) {
+        generateByTables(true, packageName, tableNames);
+    }
+}
